78a7208382fd02edcee47c09c6840153653329b5
John J Bomhold <jbomhold3@gmail.com>
2019-07-02 16:01:25 -0500

    first commit

---
+++ src/BlazorDragable/Models/TopAndLeft.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorDragable.Models
+{
+    public class TopAndLeft
+    {
+        public long Top { get; set; }
+        public long Left { get; set; }
+    }
+}
---
+++ src/BlazorDragable/wwwroot/BlazorDragable.js
@@ -0,0 +1,17 @@
+﻿window.BlazorDragable = {
+    getOffsets: function (id) {
+        var results = { Top: document.getElementById(id).offsetTop, Left: document.getElementById(id).offsetLeft }
+        return JSON.stringify(results);
+    },
+    getBoundingClientRect: function (id) {
+        var rect = document.getElementById(id).getBoundingClientRect();
+        var results = { Top: Math.round(rect.top), Left: Math.round(rect.left) }
+        return JSON.stringify(results);
+    },
+    getSize: function (id) {
+        var el = document.getElementById(id);
+
+        var results = { Width: Math.round(el.offsetWidth), Height: Math.round(el.offsetHeight) }
+        return JSON.stringify(results);
+    },
+};
